3bb4f333328925e325c23c427dd8782b5725006e
AvetNET <babuliyan@gmail.com>
2023-04-04 16:17:52 +0300

    scattered in folders

--- bot_telegram.py
+++ bot_telegram.py
@@ -1,63 +1,13 @@
-import string
-
-from aiogram import Bot, types
-from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
-from decouple import config
-import os, json
-
-bot = Bot(token=config('TOKEN'))
-dp = Dispatcher(bot)
+from create_bot import dp
+from handlers import client, admin, other
 
 
 async def on_startup(_):
     print('Бот вышел в онлайн')
 
 
-'''*********************КЛИЕНТСКАЯ ЧАСТЬ**********************************'''
-
-
-@dp.message_handler(commands=['start', 'help'])
-async def command_start(message: types.Message):
-    try:
-        await bot.send_message(message.from_user.id, 'Приятного аппетита')
-        await message.delete()
-    except:
-        await message.reply('Общение с ботом через ЛС, '
-                            'напишите ему:\nhttps://t.me/bat_bot_bit_bot.')
-
-
-@dp.message_handler(commands=['Режим_работы'])
-async def pizza_open_command(message: types.Message):
-    await bot.send_message(message.from_user.id, 'Вс-Чт с 9:00 до 20:00, '
-                                                 'Пт-Сб с 10:00 до 23:00')
-
-
-@dp.message_handler(commands=['Расположение'])
-async def pizza_open_command(message: types.Message):
-    await bot.send_message(message.from_user.id, 'ул. Колбасная 15')
-
-'''**********************АДМИНСКАЯ ЧАСТЬ**********************************'''
-
-'''************************ОБЩАЯ ЧАСТЬ************************************'''
-
-
-@dp.message_handler()
-async def echo_send(message: types.Message):
-    if {i.lower().translate(str.maketrans('', '', string.punctuation)) for i in message.text.split(' ')}.intersection(set(json.load(open('cenz.json')))) != set():
-        await message.reply('Маты запрещены')
-        await message.delete()
-
-
-
-    # if message.text == 'Привет':
-    #     await message.answer('И тебе привет')
-    # else:
-    #     await message.answer('Иди на х...')
-    #
-    # # await message.answer(message.text)
-    # # await message.reply(message.text)
-    # # await bot.send_message(message.from_user.id, message.text)
-
+client.register_handlers_client(dp)
+other.register_handlers_other(dp)
 
 executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
